71900edde8047ea58c428e9db7b564a447123232
chanz8144 <chanz8144@CA-SU-F106-26.sduhsd.lan>
2020-01-31 10:32:52 -0800

    Initial commit

---
+++ APCSA2020/src/project_zero/AsciiBox.java
@@ -0,0 +1,35 @@
+package project_zero;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Zoe Chan 1/30/20" );
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+
+	}
+}
