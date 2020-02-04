a4eb67e7850dcd8134774e0253f7ea0b90c44059
zoch74 <zaczac8144@gmail.com>
2020-02-03 23:52:05 -0800

    laptop

---
+++ APCSA/src/Unit0/AsciiArt.java
@@ -0,0 +1,30 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Zoe Chan" );
+		System.out.println("catepillar" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("         _________________            " );
+		System.out.println("         (    *   -   *   )         " );
+		System.out.println("         (       > >      )        " );
+		System.out.println("         (       > >      )         " );
+		System.out.println("         __________________           " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
