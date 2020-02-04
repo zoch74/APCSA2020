e65135ffdde71f3e139ad9ae1e3eb9fcebad0797
zoch74 <zaczac8144@gmail.com>
2020-02-04 10:05:26 -0800

    initial unit 3 project

---
+++ APCSA/src/Unit3/MilesPerHour.java
@@ -0,0 +1,47 @@
+package Unit3;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour()
+	{
+		setNums(0,0,0);
+		mph=0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{
+	}
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+	}
+
+	public void calcMPH()
+	{
+	}
+
+	public void print()
+	{
+	}
+	
+	//create a print or toString or both
+	
+	public String toString()
+	{
+		return "";
+	}
+}
