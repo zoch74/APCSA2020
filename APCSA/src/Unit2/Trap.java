41be63f623187efd6692fd80f884f78b09765f16
zoch74 <zaczac8144@gmail.com>
2020-02-04 00:20:16 -0800

    laptop

---
+++ APCSA/src/Unit2/Trap.java
@@ -0,0 +1,15 @@
+package Unit2;
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double baseSum = base1 + base2;
+		return (0.5 * baseSum * height);
+	}
+}
