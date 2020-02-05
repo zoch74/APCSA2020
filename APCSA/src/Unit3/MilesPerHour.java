ff86b7e38367225914113cb33fac25281d809780
zoch74 <zaczac8144@gmail.com>
2020-02-04 22:21:30 -0800

    everything works except everything is rounded with Math.ceil

--- APCSA/src/Unit3/MilesPerHour.java
+++ APCSA/src/Unit3/MilesPerHour.java
@@ -7,9 +7,9 @@ package Unit3;
 //Class -
 //Lab  -
 
-import java.util.Scanner; 
-import static java.lang.System.*;
-import static java.lang.Math.*;
+//import java.util.Scanner; 
+//import static java.lang.System.*;
+//import static java.lang.Math.*;
 
 public class MilesPerHour
 {
@@ -19,29 +19,39 @@ public class MilesPerHour
 	public MilesPerHour()
 	{
 		setNums(0,0,0);
-		mph=0.0;
+		mph = 0.0;
+		
 	}
 
 	public MilesPerHour(int dist, int hrs, int mins)
 	{
+		setNums(dist, hrs, mins);
+		
 	}
 
 	public void setNums(int dist, int hrs, int mins)
 	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
 	}
 
 	public void calcMPH()
 	{
+		double time = hours + ((double) minutes/60);
+		mph = Math.ceil(distance/time);
+		
 	}
 
 	public void print()
 	{
+		System.out.println(distance + " miles in " + hours + " hours and " + minutes + " minutes = " + (int)mph + " MPH.\n\n");
 	}
 	
 	//create a print or toString or both
 	
-	public String toString()
+	/*public String toString()
 	{
-		return "";
-	}
+		return ;
+	}*/
 }
