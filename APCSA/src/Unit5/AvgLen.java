440033bea8ecef0a526da54ae6c58ba0a229216b
zoch74 <zaczac8144@gmail.com>
2020-02-11 11:13:47 -0800

    initial unit 5 labs

---
+++ APCSA/src/Unit5/AvgLen.java
@@ -0,0 +1,13 @@
+package Unit5;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+		return 0;
+	}
+}
--- APCSA/src/Unit5/FirstLastVowel.java
+++ APCSA/src/Unit5/FirstLastVowel.java
@@ -9,6 +9,16 @@ public class FirstLastVowel
 {
    public static String go( String a )
 	{
-		return "add code here";
+	   if (a.length() > 1)
+	   {
+		   String end = (a.substring(a.length()-1)).toUpperCase();
+		   String start = (a.substring(0, 1)).toUpperCase();
+		   
+		   if ((end == "A") || (end == "E") || (end == "I") || (end == "O") || (end == "U")) {
+			   	return "yes";
+		   }
+	   }
+	   
+	   return "add code here";
 	}
 }
