4aa2207b7d129c138c682d14e57738f9238e9b71
zoch74 <zaczac8144@gmail.com>
2020-02-26 07:04:18 -0800

    finished with testcases

--- APCSA/src/Unit7/TriangleFive.java
+++ APCSA/src/Unit7/TriangleFive.java
@@ -12,23 +12,54 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter(' ');
+		setAmount(0);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String line = "";
 		String output="";
+		int ind = alphabet.indexOf(letter);
+		int j = 0;
+		//for loop for printing each line
+		for (int k = amount; k > 0; k--) {
+			j = (amount - k) + ind;
+			if (j >= 26) {
+				j = j -26;
+			}
+			for (int z = k; z>0; z--) {
+				line = line + alphabet.charAt(j);
+			}
+			
+			line = line + " ";
+		}
+		line = line + " ";
+		int len = line.length();
+		int rem = 0;
+		
+		for (int i = 0; i < amount; i++) {
+			rem = rem + i + 1;
+			output = output + line.substring(0,len -rem) + "\n";
+		}
+		
 		return output;
 	}
 }
--- APCSA/src/Unit7/TriangleFiveRunner.java
+++ APCSA/src/Unit7/TriangleFiveRunner.java
@@ -9,5 +9,19 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
+	   TriangleFive test = new TriangleFive('C', 4);
+	   System.out.println(test.toString());
+	   
+	   TriangleFive test1 = new TriangleFive('A', 5);
+	   System.out.println(test1.toString());
+	   
+	   TriangleFive test2 = new TriangleFive('B', 7);
+	   System.out.println(test2.toString());
+	   
+	   TriangleFive test3 = new TriangleFive('X', 6);
+	   System.out.println(test3.toString());
+	   
+	   TriangleFive test4 = new TriangleFive('Z', 8);
+	   System.out.println(test4.toString());
 	}
 }
