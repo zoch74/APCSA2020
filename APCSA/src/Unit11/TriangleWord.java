0110d966dc9b219f56d20d5b7b2ea9d39c355d0c
zoch74 <zaczac8144@gmail.com>
2020-03-09 02:17:44 -0700

    finished with test cases

--- APCSA/src/Unit11/TriangleWord.java
+++ APCSA/src/Unit11/TriangleWord.java
@@ -12,5 +12,16 @@ public class TriangleWord
 		
 	public static void printTriangle(String word)
 	{
+		int num = word.length();
+		String line = "";
+		String phrase = "";
+		for (int i = 0; i <= word.length(); i++) {
+			phrase = word.substring(0,i);
+			for (int j = 0; j < i; j++) {
+				line = line + phrase;
+			}
+			System.out.println(line);
+			line = "";
+		}
 	}
 }
--- APCSA/src/Unit11/TriangleWordRunner.java
+++ APCSA/src/Unit11/TriangleWordRunner.java
@@ -10,6 +10,18 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
+		Scanner keyboard = new Scanner(System.in);
+		String choice = "";
 		
+		do {
+		out.print("Enter a word :: ");
+		String word = keyboard.next();
+		
+		TriangleWord.printTriangle(word);
+		
+		out.print("Do you want to enter more sample input? ");
+		choice = keyboard.next();
+		
+		} while (choice.equals("y") || choice.equals("Y"));
 	}
 }
