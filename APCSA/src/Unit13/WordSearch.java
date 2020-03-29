383f4e4212b47f82751078d9723f271c65c0a337
zoch74 <zaczac8144@gmail.com>
2020-03-28 20:03:16 -0700

    update

--- APCSA/src/Unit13/WordSearch.java
+++ APCSA/src/Unit13/WordSearch.java
@@ -31,18 +31,19 @@ public class WordSearch
 		boolean found = false;
 		int len = w.length();
 		boolean match = true;
-		int i = 0;
-		while (match == true && c < m.length) {
+		int i = 0; //index of character within word
+		while (match == true && c+i < m.length) {
 			if (w.substring(i,i+1).equals(m[r][c])) {
 				match = true;
 				if(i == len-1) {
-					
+					found = true;
 				}
 			}
 			else {
 				match = false;
 			}
 			i++;
+			c++;
 			
 		}
 		
@@ -53,12 +54,53 @@ public class WordSearch
 
 	public boolean checkLeft(String w, int r, int c)
 	{
-		return false;
+		boolean found = false;
+		int len = w.length();
+		boolean match = true;
+		int i = 0; //index of character within word
+		while (match == true && c-i >= 0) {
+			if (w.substring(i,i+1).equals(m[r][c])) {
+				match = true;
+				if(i == len-1) {
+					found = true;
+				}
+			}
+			else {
+				match = false;
+			}
+			i++;
+			c--;
+			
+		}
+		
+		
+		
+		return found;
 	}
 
 	public boolean checkUp(String w, int r, int c)
 	{
-		return false;
+		boolean found = false;
+		int len = w.length();
+		boolean match = true;
+		int i = 0; //index of character within word
+		while (match == true && r-i >= 0) {
+			if (w.substring(i,i+1).equals(m[r][c])) {
+				match = true;
+				if(i == len-1) {
+					found = true;
+				}
+			}
+			else {
+				match = false;
+			}
+			i++;
+			r--;
+			
+		}
+		
+		
+		return found;
 	}
 
 	public boolean checkDown(String w, int r, int c)
